39acbbb96184ba6b58ac7f4270aa0e4dabd877a8
Mo2Hefny <moamensaied1@gmail.com>
2024-04-07 15:37:33 +0200

    Add header component

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import HeaderCSS from "./Header.module.css";
+import InputCSS from "./Input.module.css";
 
 const Header = () => {
   return (
     <header className={HeaderCSS.header}>
-      <div className={HeaderCSS.logo}>
-        <img src="src/assets/img/logo.jpeg" alt="logo" />
+      <div className={HeaderCSS.gridContainer}>
+        <div className={HeaderCSS.logo}>
+          <img src="src/assets/img/logo.jpeg" alt="logo" />
+        </div>
+        <div className={HeaderCSS.headerTitle}>
+          <h1 className="josefin-sans-bold">Cairo University</h1>
+        </div>
+        <div className={`${InputCSS.btn} ${InputCSS.btn_1} josefin-sans-bold`}>
+          Login
+        </div>
       </div>
-      <div className="university-title"></div>
-      <div className="btn-1"></div>
     </header>
   );
 };
