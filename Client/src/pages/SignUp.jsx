93a0dca6085222fad8e3ff3c3e1f605e4262234e
Mo2Hefny <moamensaied1@gmail.com>
2024-04-08 02:29:08 +0200

    Add sign up page

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,12 +1,79 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import RegistrationCSS from "./Registration.module.css";
+import InputCSS from "../components/Input.module.css";
 
 const SignUp = ({ handleLogin }) => {
   return (
-    <>
+    <div>
       <Header />
-    </>
-  )
-}
+      <main className={RegistrationCSS.body}>
+        <div className={RegistrationCSS.flexContainer_column}>
+          <h2 className={`${RegistrationCSS.title} josefin-sans-bold`}>
+            Create Account
+          </h2>
+          <div className={RegistrationCSS.gridContainer}>
+            <div
+              className={`${RegistrationCSS.flexContainer_column} ${RegistrationCSS.gridSection_small}`}
+            >
+              <label for="fname" className="josefin-sans-bold">
+                First Name
+              </label>
+              <input type="text" id="fname" className={InputCSS.inputBox} />
+            </div>
+            <div
+              className={`${RegistrationCSS.flexContainer_column} ${RegistrationCSS.gridSection_small}`}
+            >
+              <label for="lname" className="josefin-sans-bold">
+                Last Name
+              </label>
+              <input type="text" id="lname" className={InputCSS.inputBox} />
+            </div>
+            <div
+              className={`${RegistrationCSS.flexContainer_column} ${RegistrationCSS.gridSection}`}
+            >
+              <label for="email" className="josefin-sans-bold">
+                Email
+              </label>
+              <input type="email" id="email" className={InputCSS.inputBox} />
+            </div>
+            <div
+              className={`${RegistrationCSS.flexContainer_column} ${RegistrationCSS.gridSection}`}
+            >
+              <label for="password" className="josefin-sans-bold">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className={InputCSS.inputBox}
+              />
+            </div>
+            <div
+              className={`${RegistrationCSS.flexContainer_column} ${RegistrationCSS.gridSection}`}
+            >
+              <label for="cpassword" className="josefin-sans-bold">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="cpassword"
+                className={InputCSS.inputBox}
+              />
+            </div>
+          </div>
+          <div className={`${RegistrationCSS.btnSection}`}>
+            <button
+              id="signup-btn"
+              className={`${InputCSS.btn} ${InputCSS.btn_1} josefin-sans-bold`}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
