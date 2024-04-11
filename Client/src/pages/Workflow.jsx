0026823fd6a11b7bcb505d49c3e7108f51917b55
Mo2Hefny <moamensaied1@gmail.com>
2024-04-11 16:18:36 +0200

    style: change color palette and general styles

---
+++ Client/src/pages/Workflow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import UiCSS from './UI.module.css';
+
+const Workflow = () => {
+  
+  return (
+    <div className={UiCSS.body}>
+    </div>
+  );
+};
+
+export default Workflow;
